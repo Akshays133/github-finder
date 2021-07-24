a8d6b13515ef322be6713024fc510c8c3594ad61
Akshay Sharma <Akshays1333@gmail.com>
2021-07-24 13:30:41 +0530

    c

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+        <div className="App">
+          <Navbar />
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
