b05bb941b89dcc42563a3a0fd5574e24ec70cc0f
Akshay Sharma <Akshays1333@gmail.com>
2021-07-24 14:03:06 +0530

    latest context update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar'
+import GithubState from './components/context/github/GithubState';
+import AlertState from './components/context/alert/AlertState';
+
 import './App.css';
 
-class App extends Component {
-  render(){
-    return (
+
+const App = () => {
+
+  return (
+    <GithubState>
+      <AlertState>
+      <Router>
         <div className="App">
           <Navbar />
-        <h1>Hello World</h1>
+          <div className='container'>
+            <Alert />
+            <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/user/:login' component={User} />
+            <Route component={NotFound} />
+            </Switch>
+          </div>
       </div>
-    );
-  }
+      </Router>
+      </AlertState>
+    </GithubState>
+  );
 }
+// async componentDidMount() {
+//   this.setState({ loading: true });
+
+//   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+//   this.setState({ users: res.data, loading: false })
+// };
 
 export default App;
